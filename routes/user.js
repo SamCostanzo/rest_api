d294bb0fd9fd399dd50c4dfbbfe15e83a2bcec03
Sam Costanzo <samcostanzo25@gmail.com>
2019-12-04 15:42:58 -0500

    So close!

--- routes/user.js
+++ routes/user.js
@@ -22,7 +22,7 @@ function asyncHandler(cb) {
   }
 
 
-  const authenticateUser = (req, res, next) => {
+  const authenticateUser = asyncHandler(async (req, res, next) => {
     let message = null;
   
     // Parse the user's credentials from the Authorization header.
@@ -32,9 +32,16 @@ function asyncHandler(cb) {
     // If the user's credentials are available...
     if (credentials) {
       // Attempt to retrieve the user from the database by their username (i.e. the user's "key" from the Authorization header).
-      // const user = users.find(u => u.username === credentials.name); // emailAddress or username??? 
-      const user = User.find(u => u.username === credentials.name);
       
+      const user = await User.findOne({
+        where: {
+          emailAddress: credentials.name,
+          // password: credentials.password
+        }
+      });
+
+      
+
       // If a user was successfully retrieved from the data store...
       if (user) {
         // Use the bcryptjs npm package to compare the user's password (from the Authorization header) to the user's password that was retrieved from the data store.
@@ -42,12 +49,12 @@ function asyncHandler(cb) {
   
         // If the passwords match...
         if (authenticated) {
-          console.log(`Authentication successful for username: ${user.username}`);
+          console.log(`Authentication successful for username: ${user.emailAddress}`);
   
           // Then store the retrieved user object on the request object so any middleware functions that follow this middleware function will have access to the user's information.
           req.currentUser = user;
         } else {
-          message = `Authentication failure for username: ${user.username}`;
+          message = `Authentication failure for username: ${user.emailAddress}`;
         }
       } else {
         message = `User not found for username: ${credentials.name}`;
@@ -67,7 +74,7 @@ function asyncHandler(cb) {
       // Call the next() method.
       next();
     }
-  };
+  });
 
 
 
@@ -77,23 +84,25 @@ function asyncHandler(cb) {
 
 // Returns the currently authenticated user - 200 - PH - For now it just returns all the users - NEEDS WORK
 router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
-    // const user = await User.findAll();
-    // res.json(user);
     
-    const user = req.currentUser;
+  const user = req.currentUser;
 
-    res.json({
-      name: user.name,
-      username: user.username,
-    });
-    
-    res.status(200).end();
+    res.status(200).json({
+      emailAddress: user.emailAddress,
+      firstName: user.firstName,
+      lastName: user.lastName
+    }).end();
 }));
 
 
 
 // Creates a user, sets the location header to '/', and returns no content - 201 - DONE I THINK
 router.post('/users', asyncHandler(async (req, res) => {
+    
+  // if (user.password) {
+  //   user.password = bcryptjs.hashSync(user.password, 10);
+  // }
+  
     const user = await User.create(req.body); // Create a new user with the requests body
     user.password = bcryptjs.hashSync(user.password); // Hash the new users password
     res.status(201).location('/').end(); // Sets the status code, location, and then ends the response
